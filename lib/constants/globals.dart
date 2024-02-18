fb7e40c0e0a5a0c6939026e957ebce91f28dc204
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-02-18 16:20:07 -0500

    Space-ship joystick movement added.

--- lib/constants/globals.dart
+++ lib/constants/globals.dart
@@ -3,6 +3,9 @@ class Globals {
 
   static const String backgroundSprite = 'background1920x1080.png';
   static const String spaceShipSprite = 'space-ship.png';
+  static const String spaceShipLeft = 'space-ship-leftSlide.png';
+  static const String spaceShipRight = 'space-ship-rightSlide.png';
+  static const String spaceShipDown = 'space-ship-downSlide.png';
 
   // static const String backgroundSprite = 'background1080x1920.png';
 }
